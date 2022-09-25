758cbf5d491e3eda57ae164ead1f1786f36e0732
Jose Sosa <josesosa2001@gmail.com>
2022-09-25 13:55:50 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/podplay/model/Podcast.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.podplay.model
+
+import java.util.*
+
+data class Podcast(
+    var feedUrl: String = "",
+    var feedTitle: String = "",
+    var feedDesc: String = "",
+    var imageUrl: String = "",
+    var lastUpdated: Date = Date(),
+    var episodes: List<Episode> = listOf()
+)
+
+// provides data for the podcast itself
